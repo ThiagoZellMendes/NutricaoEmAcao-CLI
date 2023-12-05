2a2b8c9e7576e7f9a4a9d4286a528eb0d5a40342
Thiago Mendes <thiagosmendes.th@gmail.com>
2023-12-05 12:44:12 -0300

    Chore: Global-Props-Themes

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,37 @@
+export default {
+  colors: {
+    primary: '#2F9E44',
+
+    secondary: '#FF872C',
+    secondary_light: 'rgba(255,135,44,0.3)',
+
+    success: '#12A454',
+    success_light: 'rgba(18,164,84,0.5)',
+    
+    attention: '#E83D5B',
+    attention_light: 'rgba(232,63,91,0.5)',
+
+    genreMan:"#2f80ed",
+    genreWoman: "#9b51e0",
+
+    shape: '#FFFFFF',
+    title: '#65717B',
+    title_dark: '#343A40',
+    text: '#9AA4AC',
+    background: '#F0F2F5',
+
+   MuitoBaixo: '#D2691E',
+   Excelente:'#12A454',
+   MuitoBom:'#FFD700',
+   Bom:'#2f80ed',
+   Adequado:'#2f80ed',
+   ModeradamenteAlto:'#B22222',
+   Alto:'#FF7F50',
+   MuitoAlto:'	#FF0000',
+
+  },
+
+
+}
+    
+    
---
+++ src/screens/globalProps.ts
@@ -0,0 +1,17 @@
+export interface PatientProps {
+  key: string
+  fullName?: string
+  cpf?: string
+  age: string
+  genre: string
+  weight?: string
+  height?: string
+  imc?: number
+  diagnosticImc?: string
+  triceps?: string
+  biceps?: string
+  subescapular?: string
+  supraIliaca?: string
+  pgc?: number
+  categoryPcg?: string
+}
