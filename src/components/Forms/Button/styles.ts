cb14b9d1201534083fed6249c85252b142bbb6b6
Thiago Mendes <thiagosmendes.th@gmail.com>
2023-12-05 12:51:32 -0300

    Feat: Form-Button

---
+++ src/components/Forms/Button/styles.ts
@@ -0,0 +1,16 @@
+import { RectButton } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  padding: 18px;
+  border-radius: 5px;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`;
