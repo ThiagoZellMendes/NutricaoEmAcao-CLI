0ec397c25f12f8ab37b3e7abc34902ec5449c3cc
Thiago Mendes <thiagosmendes.th@gmail.com>
2023-12-12 13:53:33 -0300

    Chore: Initialize-firebase-storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import "react-native-gesture-handler"
-import { useEffect } from "react"
-import { StatusBar } from "react-native"
-import { ThemeProvider } from "styled-components/native"
+import { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import 'react-native-gesture-handler';
+import { ThemeProvider } from 'styled-components/native';
 
-import { RoutersApp } from "./routes/app.routers"
+import { RoutersApp } from './routes/app.routers';
 
-
-import firebase from "@react-native-firebase/app"
-import { firebaseConfig } from "./services/fireBaseConfig"
-import "@react-native-firebase/firestore";
-import theme from "./global/styles/theme"
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/firestore';
+import '@react-native-firebase/storage';
+import theme from './global/styles/theme';
+import { firebaseConfig } from './services/fireBaseConfig';
 
 export default function App() {
- 
   useEffect(() => {
     if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig)
+      firebase.initializeApp(firebaseConfig);
     }
-  }, [])
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <StatusBar />
       <RoutersApp />
-   
     </ThemeProvider>
-  )
+  );
 }
